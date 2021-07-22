60a8b4702c254168b65a5f48fd6f45b3bbccc100
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2021-07-22 15:34:39 +0700

    many changes

--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -50,7 +50,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
   private static final Logger LOGGER = new Logger();
 
   // Configuration values for the prepackaged SSD model.
-  private static final int TF_OD_API_INPUT_SIZE = 300;
+  private static final int TF_OD_API_INPUT_SIZE = 320;
   private static final boolean TF_OD_API_IS_QUANTIZED = true;
   private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
   private static final String TF_OD_API_LABELS_FILE = "labelmap.txt";
@@ -77,6 +77,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
 
   private Matrix frameToCropTransform;
   private Matrix cropToFrameTransform;
+  private Matrix unmirror;
 
   private MultiBoxTracker tracker;
 
@@ -116,7 +117,7 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
     previewWidth = size.getWidth();
     previewHeight = size.getHeight();
 
-    sensorOrientation = rotation - getScreenOrientation();
+    sensorOrientation = (rotation - getScreenOrientation())*-1;
     LOGGER.i("Camera orientation relative to screen canvas: %d", sensorOrientation);
 
     LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
